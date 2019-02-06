7b498aa5d9c8c81a36ada16603ac4a7c7c2bd8a8
theWebalyst <github@happybeing.com>
2019-02-06 17:16:46 +0000

    Prevent wasteful renaming when deploying to safe (rsync -u -> cp -u)

--- scripts/deployweb.sh
+++ scripts/deployweb.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script can be used to deploy your website from
 # the dist/ directory directly to your hosting service
@@ -14,16 +14,26 @@
 #   modify the source and destination paths below
 #   ./scripts/deployweb.sh
 
-SOURCE='./dist/*' # Directory holding production website
-DESTINATION='/'   # Upload directory on ftp account
+set -e  # Exit on error
+set +v  # Don't echo output
+SOURCE='./dist'  # Directory holding production website
+FTP_DIR='/'   # Upload directory on ftp account
 
 yarn build
 echo FTP upload...
-ncftpput -R -f scripts/rsdemo.ncftp $DESTINATION $SOURCE
+# ncftpput -R -f scripts/rsdemo.ncftp $FTP_DIR $SOURCE/*
 
-exit 0  # Remove this line to enable the following
+#exit 0  # Remove this line to enable deploy to SAFE Network
 # Deploy via SAFE Drive to an existing SAFE Network public name (website)
-SAFE_DESTINATION='~/SAFE/_public/rsdemo/root-www/'
-read -p "Mount SAFE Drive and press ENTER to sync...  "
+SAFE_DIR=~/SAFE/_public/dweb/root-www
+if [ "$1" == "mock" ]; then
+  SAFE_DIR=~/SAFE/_public/tests/data1/dweb
+  mkdir -p $SAFE_DIR
+fi
+
+read -p "Mount SAFE Drive and press ENTER to sync..."
 echo Syncing via SAFE Drive...
-rsync -av --delete $SOURCE/ $SAFE_SAFE_DESTINATION
+UPLOAD=$SOURCE-upload
+rsync -rc --delete $SOURCE/ $UPLOAD/ && \
+cp -ruv $UPLOAD/* $SAFE_DIR/ && \
+rsync -ru --delete $UPLOAD/ $SAFE_DIR/
